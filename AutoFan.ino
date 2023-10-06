af2b825fcd76f70400e587d05c19f048c3174ae1
kirby-b <118732723+kirby-b@users.noreply.github.com>
2023-10-06 11:55:37 -0400

    Create AutoFan.ino

---
+++ AutoFan.ino
@@ -0,0 +1,11 @@
+// Put initial variables here
+
+// Put method to turn on and off fan here
+
+//Temp method here
+
+//Humidity method here
+
+//Put detection method here that checks tempurature/humidity in their methods
+
+//Optional methods here that control buttons, lights, and/or screens
